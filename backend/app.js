fcf62631b1066ef15e77f7862d2202548bf294aa
leshafun <leshafun19@yandex.ru>
2022-12-12 20:53:45 +0700

    PR15c

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,13 @@
-// require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
 const { errors, celebrate, Joi } = require('celebrate');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
 const { auth } = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const NotFound = require('./errors/NotFound');
-const cors = require('./middlewares/cors');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
@@ -18,16 +15,6 @@ const app = express();
 
 app.listen(3000);
 app.use(express.json());
-
-app.use(cors);
-
-app.use(requestLogger);
-
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
 
 app.use('/users', auth, userRouter);
 app.use('/cards', auth, cardRouter);
@@ -53,8 +40,6 @@ app.use((req, res, next) => {
   const error = new NotFound('Страница не найдена');
   next(error);
 });
-
-app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
