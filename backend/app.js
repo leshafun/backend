5058a57851e05fbad0c82df4aa66757f6cc6cd08
leshafun <leshafun19@yandex.ru>
2022-12-14 20:59:28 +0700

    15NEW

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ const { login, createUser } = require('./controllers/users');
 const { auth } = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const NotFound = require('./errors/NotFound');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
 
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
@@ -17,6 +19,15 @@ const app = express();
 app.listen(3001);
 app.use(express.json());
 
+app.use(cors);
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.use('/users', auth, userRouter);
 app.use('/cards', auth, cardRouter);
 
@@ -41,6 +52,8 @@ app.use((req, res, next) => {
   const error = new NotFound('Страница не найдена');
   next(error);
 });
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
