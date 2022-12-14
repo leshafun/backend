d7ac35ab13f759ab13c2ef1b9a13c70bc32168bd
leshafun <98995708+leshafun@users.noreply.github.com>
2022-12-14 13:10:44 +0700

    Delete backend directory

--- backend/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-
-  res.status(statusCode).send({ message: statusCode === 500 ? 'Произошла ошибка' : message });
-  next();
-};
-
-module.exports = errorHandler;
--- backend/utils/constants.js
+++
@@ -1,7 +0,0 @@
-const SUCCESS_OK = 200;
-const CREATED = 201;
-
-module.exports = {
-  SUCCESS_OK,
-  CREATED,
-};
