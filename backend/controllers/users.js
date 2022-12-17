027d62f325259768cd298766f72ae347b8d0922b
leshafun <leshafun19@yandex.ru>
2022-12-17 21:29:26 +0700

    15old

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -6,7 +6,6 @@ const BadRequest = require('../errors/BadRequest');
 const NotFound = require('../errors/NotFound');
 const { SUCCESS_OK } = require('../utils/constants');
 const { CREATED } = require('../utils/constants');
-
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 // возвращает всех пользователей
@@ -72,8 +71,7 @@ const createUser = (req, res, next) => {
             next(err);
           }
         });
-    })
-    .catch(next);
+    });
 };
 
 // обновляет профиль
@@ -140,7 +138,7 @@ const getUserInfo = (req, res, next) => {
   User.findById(_id)
     .then((user) => {
       if (!user) {
-        return next(new NotFound('Пользователь не найден'));
+        return next(new Error('Пользователь не найден'));
       }
       return res.status(SUCCESS_OK).send({ data: user });
     })
