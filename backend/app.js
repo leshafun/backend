78a501a8c51522ba0dea16a7d52f09215c594567
leshafun <leshafun19@yandex.ru>
2022-12-17 19:32:40 +0700

    15NEW

--- backend/app.js
+++ backend/app.js
@@ -66,7 +66,7 @@ app.post('/signup', celebrate({
   }),
 }), createUser);
 
-app.use((req, res, next) => {
+app.use('*', (req, res, next) => {
   const error = new NotFound('Страница не найдена');
   next(error);
 });
