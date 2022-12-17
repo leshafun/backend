b84bd0f9a38e48f1794e53f776ad504a80d4d638
leshafun <leshafun19@yandex.ru>
2022-12-17 19:17:02 +0700

    15NEW

--- backend/app.js
+++ backend/app.js
@@ -60,7 +60,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/^https?:\/\/[a-z0-9~_\-\.]+\.[a-z]{2,9}([a-z0-9\[\]\#\-\.\_\~\/\?\@\!\$\&\'\(\)\*\+\,\;\:\=]*)?$/i),
+    avatar: Joi.string().pattern(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)$/),
     email: Joi.string().min(3).required().email(),
     password: Joi.string().required(),
   }),
