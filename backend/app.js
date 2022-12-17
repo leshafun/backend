686fda3cb2325d3f2e36688b7d9c5a175fd74048
leshafun <leshafun19@yandex.ru>
2022-12-17 19:12:34 +0700

    15NEW

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,6 @@ const { auth } = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const NotFound = require('./errors/NotFound');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const BadRequest = require('./errors/BadRequest');
 // const cors = require('./middlewares/cors');
 
 const cors = require('cors');
@@ -61,12 +60,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().custom((value) => {
-      if (!validator.isURL(value, { require_protocol: true })) {
-        throw new BadRequest('Неправильный формат URL адреса');
-      }
-      return value;
-    }),
+    avatar: Joi.string().pattern(/^https?:\/\/[a-z0-9~_\-\.]+\.[a-z]{2,9}([a-z0-9\[\]\#\-\.\_\~\/\?\@\!\$\&\'\(\)\*\+\,\;\:\=]*)?$/i),
     email: Joi.string().min(3).required().email(),
     password: Joi.string().required(),
   }),
