e3f550abc350403de8595c330c90c5bfbc8d532d
leshafun <leshafun19@yandex.ru>
2022-12-17 19:36:18 +0700

    15NEW

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 const app = express();
 
-app.use('*', cors(options));
+app.use(cors(options));
 
 app.listen(3000);
 app.use(express.json());
